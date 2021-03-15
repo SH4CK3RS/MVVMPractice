c210f18251c40b7301781809df69dd3a99754bbf
ByeonggeunSon <qudrms007@naver.com>
2021-03-16 00:35:15 +0900

    feat: MoviesListTableViewController 기본 코드 추가

--- MVVM/Presentation/MovieScene/MovieList/View/MoviesListTableView/MoviesListTableViewController.swift
+++ MVVM/Presentation/MovieScene/MovieList/View/MoviesListTableView/MoviesListTableViewController.swift
@@ -5,4 +5,42 @@
 //  Created by 손병근 on 2021/03/16.
 //
 
-import Foundation
+import UIKit
+
+class MoviesListTableViewController: UITableViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    registerCell()
+    setupViews()
+  }
+  
+  func reload() {
+    tableView.reloadData()
+  }
+  
+  private func registerCell() {
+    self.tableView.register(MoviesListItemCell.self, forCellReuseIdentifier: MoviesListItemCell.reuseIdentifier)
+  }
+  
+  private func setupViews() {
+    tableView.estimatedRowHeight = MoviesListItemCell.height
+    tableView.rowHeight = UITableView.automaticDimension
+  }
+}
+
+// MARK: - TableViewDataSource, TableViewDelegate Implementation
+
+extension MoviesListTableViewController {
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: MoviesListItemCell.reuseIdentifier, for: indexPath) as? MoviesListItemCell else { assertionFailure("Cannot dequeue reusable cell \(MoviesListItemCell.self) with reuseIdentifier: \(MoviesListItemCell.reuseIdentifier)")
+      return UITableViewCell()
+    }
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    <#code#>
+  }
+}
