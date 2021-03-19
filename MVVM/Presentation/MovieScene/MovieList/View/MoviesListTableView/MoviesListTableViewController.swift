398287069a38f1a42bdad7ea32d570634d0962a7
Steve <steve_son1015@naver.com>
2021-03-20 00:05:03 +0900

    feat: MoviesListTableviewController - Repository 추가 및 cell fill 적용

--- MVVM/Presentation/MovieScene/MovieList/View/MoviesListTableView/MoviesListTableViewController.swift
+++ MVVM/Presentation/MovieScene/MovieList/View/MoviesListTableView/MoviesListTableViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class MoviesListTableViewController: UITableViewController {
   
+  var viewModel: MoviesListViewModel!
+  
+  var posterImagesRepository: PosterImagesRepository?
+  var nextPageLoadingSpinner: UIActivityIndicatorView?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     registerCell()
@@ -18,6 +23,19 @@ class MoviesListTableViewController: UITableViewController {
   func reload() {
     tableView.reloadData()
   }
+  
+  func updateLoading(_ loading: MoviesListViewModelLoading?) {
+    switch loading {
+    case .nextPage:
+      nextPageLoadingSpinner?.removeFromSuperview()
+      nextPageLoadingSpinner = makeActivityIndicator(size: .init(width: tableView.frame.width, height: 44))
+      tableView.tableFooterView = nextPageLoadingSpinner
+    case .fullScreen, .none:
+      tableView.tableFooterView = nil
+    }
+  }
+  
+  // MARK: - Private
   
   private func registerCell() {
     self.tableView.register(MoviesListItemCell.self, forCellReuseIdentifier: MoviesListItemCell.reuseIdentifier)
@@ -32,15 +50,32 @@ class MoviesListTableViewController: UITableViewController {
 // MARK: - TableViewDataSource, TableViewDelegate Implementation
 
 extension MoviesListTableViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.items.value.count
+  }
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: MoviesListItemCell.reuseIdentifier, for: indexPath) as? MoviesListItemCell else { assertionFailure("Cannot dequeue reusable cell \(MoviesListItemCell.self) with reuseIdentifier: \(MoviesListItemCell.reuseIdentifier)")
       return UITableViewCell()
+    }
+    
+    cell.fill(
+      with: viewModel.items.value[indexPath.row],
+      posterImagesRepository: posterImagesRepository
+    )
+    
+    if indexPath.row == viewModel.items.value.count - 1 {
+      viewModel.didLoadNextPage()
     }
     
     return cell
   }
   
   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    <#code#>
+    return viewModel.isEmpty ? tableView.frame.height : super.tableView(tableView, heightForRowAt: indexPath)
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    viewModel.didSelectItem(at: indexPath.row)
   }
 }
