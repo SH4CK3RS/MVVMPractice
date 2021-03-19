0592c517c89ada7947f3e03764d0fa54bd24194d
ByeonggeunSon <qudrms007@naver.com>
2021-03-19 15:17:07 +0900

    feat: MovieListItemViewModel추가

---
+++ MVVM/Presentation/MovieScene/MovieList/ViewModel/MovieListItemViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  MovieListItemViewModel.swift
+//  MVVM
+//
+//  Created by 손병근 on 2021/03/19.
+//
+
+import Foundation
+
+struct MovieListItemViewModel {
+  let title: String
+  let overview: String
+  let releaseDate: String
+  let posterImagPath: String?
+}
+
+extension MovieListItemViewModel {
+  init(movie: Movie) {
+    self.title = movie.title ?? ""
+    self.posterImagPath = movie.posterPath
+    self.overview = movie.overview ?? ""
+    if let releaseDate = movie.releaseDate {
+      self.releaseDate = "\(NSLocalizedString("ReleaseDate", comment: "")): \(dateFormatter.string(from: releaseDate))"
+    } else {
+      self.releaseDate = NSLocalizedString("To be announced", comment: "")
+    }
+  }
+}
+
+private let dateFormatter: DateFormatter = {
+  let formatter = DateFormatter()
+  formatter.dateStyle = .medium
+  return formatter
+}()
---
+++ MVVM/Presentation/MovieScene/MovieList/ViewModel/MovieListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieListViewModel.swift
+//  MVVM
+//
+//  Created by 손병근 on 2021/03/19.
+//
+
+import Foundation
